7ab0dfd7f8e059d3e214b6c86a9ba49732881198
Maxwell Walter <vasuul@xulne.com>
2016-08-13 23:54:54 -0400

    Added initial panel driver code

--- ip_repo/panel_driver_1.0/hdl/panel_driver.vhd
+++ ip_repo/panel_driver_1.0/hdl/panel_driver.vhd
@@ -24,47 +24,143 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity panel_driver is
-    Port ( clk : in STD_LOGIC;
-           clk_out : out STD_LOGIC;
-           reset : in std_logic;
-           buf_in : in STD_LOGIC_VECTOR (31 downto 0);
-           buf_addr : out STD_LOGIC_VECTOR (11 downto 0);
-           R : out STD_LOGIC_VECTOR (1 downto 0);
-           G : out STD_LOGIC_VECTOR (1 downto 0);
-           B : out STD_LOGIC_VECTOR (1 downto 0));
-end panel_driver;
+-- assume the input clock is 50MHz
+entity led_clock is
+  Port ( clk   : in std_logic;
+         reset : in std_logic;
+         
+         LED_OE : out std_logic;
+         
+         R0, R1 : out std_logic;
+         G0, G1 : out std_logic;
+         B0, B1 : out std_logic;
+         
+         LED_CLK   : out std_logic;
+         LED_LATCH : out std_logic
+         );
+end led_clock;
 
-architecture Behavioral of panel_driver is
-  signal clk_counter : integer;
-  signal clk_out_int : std_logic;
-  signal buf_addr_int : std_logic_vector(11 downto 0);
+architecture Behavioral of led_clock is
+  -------- signals for heartbeat --------
+  signal counter : integer range 0 to 50000000;
+  signal hb : std_logic;
+
+  -------- signals for panel driver --------
+  -- Address for the output latch
+  signal curr_addr : integer range 0 to 7;  -- 8 scanlines
+  signal curr_bit  : integer range 0 to 31; -- 32 bits
+
+  type PANEL_STATE is (INIT, SHIFTING_LOW, SHIFTING_HIGH, LINE_DONE, PANEL_DONE);
+  signal curr_state : PANEL_STATE;
+
+  signal red0   : std_logic_vector(31 downto 0);
+  signal blue0  : std_logic_vector(31 downto 0);
+  signal green0 : std_logic_vector(31 downto 0);
+
+  signal red1   : std_logic_vector(31 downto 0);
+  signal blue1  : std_logic_vector(31 downto 0);
+  signal green1 : std_logic_vector(31 downto 0);
 begin
 
-process (clk)
+process(clk)
 begin
-  if(reset = '1') then
-    clk_counter <= 0;
-    clk_out_int <= '0';
-    buf_addr_int <= (others => '0');
-  elsif rising_edge(clk) then 
-    clk_counter <= clk_counter + 1;
-    
-    if(clk_counter = 3) then
-      clk_out_int <=  not clk_out_int;
-      clk_counter <= 0;
-    end if;
-  end if;
+  if(clk'event and clk = '1') then
+    if(reset = '1') then
+      LED_OE <= '0';
+      LED_CLK <= '0';
+      LED_LATCH <= '0';
+      
+      curr_addr <= 0;
+      curr_bit <= 0;
+
+      red0   <= X"ABCD0123";
+      green0 <= X"8901CDEF";
+      blue0  <= X"6789EFAB";
+      
+      red1   <= X"4567EFAB";
+      green1 <= X"2345ABCD";
+      blue1  <= X"0123CDEF";
+      
+      curr_state <= SHIFTING_LOW;
+    else
+      case curr_state is
+        when INIT =>
+          LED_OE <= '1';
+          -- TODO: Load new values for colors
+          -- New plan, On each new bit, make sure I read in
+          --  the pixel value for that element.  Then we can set
+          --  the bit based on that number.  We should not try to
+          --  front calculate anything.  That will be too slow
+          red0   <= X"ABCD0123";
+          green0 <= X"8901CDEF";
+          blue0  <= X"6789EFAB";
+          
+          red1   <= X"4567EFAB";
+          green1 <= X"2345ABCD";
+          blue1  <= X"0123CDEF";
+          
+          curr_state <= SHIFTING_LOW;
+          
+        when SHIFTING_LOW =>
+          LED_CLK <= '0';
+          
+          R0 <= red0(0);   -- Upper scanline first
+          G0 <= green0(0);
+          B0 <= blue0(0);
+
+          R1 <= red1(0);   -- Then the lower scanline
+          G1 <= green1(0);
+          B1 <= blue1(0);
+
+          curr_state <= SHIFTING_HIGH;
+          
+        when SHIFTING_HIGH =>
+          LED_CLK <= '1';
+          
+          red0   <= red0(0)   & red0(31 downto 1);
+          green0 <= green0(0) & green0(31 downto 1);
+          blue0  <= blue0(0)  & blue0(31 downto 1);
+
+          red1   <= red1(0)   & red1(31 downto 1);
+          green1 <= green1(0) & green1(31 downto 1);
+          blue1  <= blue1(0)  & blue1(31 downto 1);
+
+          if(curr_bit = 31) then
+            curr_state <= LINE_DONE;
+            LED_LATCH <= '1';
+          else
+            curr_state <= SHIFTING_LOW;
+          end if;
+
+          curr_bit <= curr_bit + 1;
+
+        when LINE_DONE =>
+          LED_LATCH <= '0';
+          LED_CLK <= '0';
+
+          if(curr_addr = 7) then
+            curr_state <= PANEL_DONE;
+          else
+            curr_state <= INIT;
+          end if;
+
+          curr_bit <= 0;
+          curr_addr <= curr_addr + 1;
+          
+        when PANEL_DONE =>
+          curr_addr <= 0;
+          curr_state <= INIT;
+          
+      end case;
+    end if; -- end reset else
+  end if; -- end clk'event
 end process;
-
-clk_out <= clk_out_int;
-buf_addr <= buf_addr_int;
-
+                   
 end Behavioral;
